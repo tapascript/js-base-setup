c9682ce739f56694b96cf1118d03f4de64824ed4
Shyam Mahanta <smahanta118@gmail.com>
2025-05-25 19:14:22 +0530

    JS Base setup with vite

---
+++ src/main.js
@@ -0,0 +1,9 @@
+const el = document.getElementById("app");
+
+el.innerHTML = `
+  <h1>Congratulations! Your Setup is Done</h1>
+   <p class="text-sm my-8">
+        Made with ❤️ by
+        <a href="https://tapascript.io" target="_blank">tapaScript</a>
+        community
+      </p>`;
